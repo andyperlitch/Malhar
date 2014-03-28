eb2201b276cd1318074112c54966e5e3b65d3354
Andy Perlitch <andyperlitch@gmail.com>
2014-03-28 10:27:01 -0700

    added mock api, moved breadcrumbs to ops and dev only

--- ng-console/app/scripts/controllers/navbar-top.js
+++ ng-console/app/scripts/controllers/navbar-top.js
@@ -1,8 +1,6 @@
 'use strict';
 
 angular.module('ngConsoleApp')
-  .controller('NavbarTopCtrl', function ($scope) {
-    $scope.showLicenseInfo = function() {
-        console.log('showLicenseInfo');
-    };
+  .controller('NavbarTopCtrl', function () {
+    
   });
