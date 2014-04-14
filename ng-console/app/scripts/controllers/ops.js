669815282299840cbd2cd993b0422e1ea6d87c48
Andy Perlitch <andyperlitch@gmail.com>
2014-04-14 10:37:47 -0700

    added field definitions from old code (incompatibilities)

--- ng-console/app/scripts/controllers/ops.js
+++ ng-console/app/scripts/controllers/ops.js
@@ -11,7 +11,64 @@ angular.module('ngConsoleApp')
         dataModelType: OverviewDataModel,
         dataModelOptions: {
           fields: [
-
+            {
+              label: DT.text('cores_label'),
+              key: 'cpuPercentage',
+              value: function(cpuPercentage) {
+                if (!cpuPercentage) {
+                  return '-';
+                }
+                return formatters.cpusFormatter(cpuPercentage, true);
+              }
+            },
+            {
+              label: DT.text('current alloc mem'),
+              key: 'currentMemoryAllocatedMB',
+              value: function(currentMemoryAllocatedMB, attrs) {
+                return formatters.byteFormatter(currentMemoryAllocatedMB, 'mb');
+              }
+            },
+            {
+              label: DT.text('peak alloc mem'),
+              key: 'maxMemoryAllocatedMB',
+              value: function(maxMemoryAllocatedMB) {
+                return formatters.byteFormatter(maxMemoryAllocatedMB, 'mb');
+              }
+            },
+            {
+              label: DT.text('running / pending / failed / finished / killed / submitted'),
+              key: 'numAppsRunning',
+              value: function(numAppsRunning, attrs) {
+                return '<span class="status-running">' + formatters.commaGroups(attrs.numAppsRunning) + '</span> / ' +
+                '<span class="status-pending-deploy">' + formatters.commaGroups(attrs.numAppsPending) + '</span> / ' +
+                '<span class="status-failed">' + formatters.commaGroups(attrs.numAppsFailed) + '</span> / ' +
+                '<span class="status-finished">' + formatters.commaGroups(attrs.numAppsFinished) + '</span> / ' +
+                '<span class="status-killed">' + formatters.commaGroups(attrs.numAppsKilled) + '</span> / ' +
+                '<span class="status-submitted">' + formatters.commaGroups(attrs.numAppsSubmitted) + '</span>';
+              }
+            },
+            {
+              label: DT.text('num_containers_label'),
+              key: 'numContainers'
+            },
+            {
+              label: DT.text('num_operators_label'),
+              key: 'numOperators'
+            },
+            {
+              label: DT.text('tuples_per_sec'),
+              key: 'tuplesProcessedPSMA',
+              value: function(val) {
+                return formatters.commaGroups(val);
+              }
+            },
+            {
+              label: DT.text('emitted_per_sec'),
+              key: 'tuplesEmittedPSMA',
+              value: function(val) {
+                return formatters.commaGroups(val);
+              }
+            }
           ]
         }
       }
@@ -41,8 +98,6 @@ angular.module('ngConsoleApp')
     ];
 
     var defaultWidgets = _.clone(widgetDefinitions);
-
-    $scope.percentage = 55;
 
     $scope.dashboardOptions = {
       useLocalStorage: false, //TODO enable by default
